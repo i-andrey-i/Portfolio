9c457437a0de0238f4affa3c5a1ba3a63fca5e41
shept <sheptikhinv@gmail.com>
2024-05-09 03:12:40 +0500

    started style.css rework finished Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-import logo from "../img/logo.png";
-import icon from "../img/user-icon.png";
+import {ReactComponent as Loupe} from "../img/Loupe.svg";
+import {ReactComponent as Logo} from "../img/Logo.svg";
+import {ReactComponent as ProfileIcon} from "../img/profileIcon.svg";
+import {ReactComponent as DropdownArrow} from "../img/dropdownArrow.svg";
 
 const Header = () => {
     const userId = localStorage.getItem("userId");
@@ -9,18 +11,27 @@ const Header = () => {
     return (
         <header>
             <nav className="navigation">
-                <Link to="/" class="logo"><img src={logo} alt="" width="230" height="60"/></Link>
+                <Link to="/" className="main-logo">
+                    <Logo/>
+                    <p className="main-title">MY Portfolio</p>
+                </Link>
 
                 <ul className="navigation-items">
-                    <li className="search"><input className="input" type="text" placeholder="Поиск" name="one-line"
-                                                  size="20"></input></li>
-                    <li className="link"><Link to="/"><p className="link-text">Главная</p></Link></li>
-                    <li className="link"><a href="#"><p className="link-text">Проекты</p></a></li>
+                    <li className="search-with-icon loupe nav-item">
+                        <input className="search input" type="text" placeholder="Поиск"></input>
+                    </li>
+                    <li className="link nav-item">
+                        <Link to="/"><p className="link-text">Главная</p></Link>
+                    </li>
+                    <li className="link nav-item">
+                        <a href="#"><p className="link-text">Проекты</p></a>
+                    </li>
                 </ul>
 
-                <Link to={userId ? `/profile/${userId}` : "/login"} class="profile-link">
-                    <img className="img-profile-link" src={icon} alt="" width="42" height="42"/>
-                    <p className="profile-link-text"> Мой профиль</p>
+                <Link to={userId ? `/profile/${userId}` : "/login"} className="button-with-icon profile-button">
+                    <ProfileIcon width="42" height="42"/>
+                    <p className=""> Мой профиль</p>
+                    <DropdownArrow/>
                 </Link>
             </nav>
         </header>
