c089261d602ba9d5e77bbcf2f5b61d3bf049addc
shept <sheptikhinv@gmail.com>
2024-05-09 13:54:52 +0500

    extracted Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,32 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from "../img/Logo.svg";
 import {ReactComponent as ProfileIcon} from "../img/profileIcon.svg";
 import {ReactComponent as DropdownArrow} from "../img/dropdownArrow.svg";
+import styles from "../css/Header.module.css";
 
 const Header = () => {
     const userId = localStorage.getItem("userId");
 
     return (
         <header>
-            <nav className="navigation">
-                <Link to="/" className="main-logo">
+            <nav className={styles.navigation}>
+                <Link to="/" className={styles.mainLogo}>
                     <Logo/>
-                    <p className="main-title">MY Portfolio</p>
+                    <p className={styles.mainTitle}>MY Portfolio</p>
                 </Link>
 
-                <ul className="navigation-items">
-                    <li className="search-with-icon loupe nav-item">
-                        <input className="search input" type="text" placeholder="Поиск"></input>
+                <ul className={styles.navigationItems}>
+                    <li className={`${styles.searchWithIcon} ${styles.loupe} ${styles.navItem}`}>
+                        <input className={`${styles.search} ${styles.input}`} type="text" placeholder="Поиск"></input>
                     </li>
-                    <li className="link nav-item">
-                        <Link to="/"><p className="link-text">Главная</p></Link>
+                    <li className={`${styles.link} ${styles.navItem}`}>
+                        <Link to="/"><p className={styles.linkText}>Главная</p></Link>
                     </li>
-                    <li className="link nav-item">
-                        <a href="#"><p className="link-text">Проекты</p></a>
+                    <li className={`${styles.link} ${styles.navItem}`}>
+                        <a href="#"><p className={styles.linkText}>Проекты</p></a>
                     </li>
                 </ul>
 
-                <Link to={userId ? `/profile/${userId}` : "/login"} className="button-with-icon profile-button">
+                <Link to={userId ? `/profile/${userId}` : "/login"} className={`${styles.buttonWithIcon} ${styles.profileButton}`}>
                     <ProfileIcon width="42" height="42"/>
                     <p className=""> Мой профиль</p>
                     <DropdownArrow/>
