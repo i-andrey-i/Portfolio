cafa9d4b9f1232ff3a7246eebe17cd0161baec02
shept <sheptikhinv@gmail.com>
2024-06-11 02:10:02 +0500

    made likes on ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,39 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import workIMG from "../img/img-test.png";
 import user from "../img/image 1 (1).png";
 import {ReactComponent as ThumbUp} from "../img/ThumbUp.svg";
 import {ReactComponent as Comment} from "../img/Comment.svg";
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import {Link} from "react-router-dom";
 import styles from "../css/ProjectCard.module.css"
+import {Favorite} from "@mui/icons-material";
+import {likeProject, unlikeProject} from "../api/ProjectApi";
 
 const ProjectCard = props => {
     const {project, showUserInfo} = props;
+    const [projectData, setProjectData] = useState(project);
     const date = new Date(project.data.created_at);
     const formattedDate = date.toLocaleDateString('ru-RU', {day: '2-digit', month: '2-digit', year: 'numeric'});
+
+    const toggleLike = () => {
+        if (projectData.is_liked) {
+            unlikeProject(project.data.id)
+                .then(data => setProjectData(data))
+                .catch(error => console.log(error));
+        }
+        else {
+            likeProject(project.data.id)
+                .then(data => setProjectData(data))
+                .catch(error => console.log(error));
+        }
+        console.log(projectData);
+    }
+
     return (
         <div className={styles.projectCard}>
             {showUserInfo && (
-                <Link to={`/profile/${project.data.user_id}`}>
+                <Link to={`/profile/${projectData.data.user_id}`}>
                     <div className={styles.userInfo}>
 
                         <img src={user} alt="" width="38" height="38"/>
-                        <p className={styles.username}>{project.data.username}</p>
+                        <p className={styles.username}>{projectData.data.username}</p>
 
                     </div>
                 </Link>
             )}
             <div className={styles.projectInfo}>
-                <p className={styles.projectTitle}>{project.data.title}</p>
+                <p className={styles.projectTitle}>{projectData.data.title}</p>
                 <img className={styles.projectPreview} src={workIMG} alt="Изображение работы"/>
-                <p className={styles.projectDescription}>{project.data.description}</p>
+                <p className={styles.projectDescription}>{projectData.data.description}</p>
                 <div className={styles.projectMeta}>
-                    <time className={styles.projectCreatedAt} dateTime={project.data.created_at}>{formattedDate}</time>
+                    <time className={styles.projectCreatedAt} dateTime={projectData.data.created_at}>{formattedDate}</time>
                     <div className={`${styles.iconWithText} ${styles.projectLikes}`}>
-                        <span>{project.data.likes_count}</span>
-                        <ThumbUp className="icon"/>
+                        <span>{projectData.data.likes_count}</span>
+                        {projectData.is_liked ?
+                            <Favorite onClick={() => toggleLike()} className={`${styles.Icon} ${styles.Like}`} style={{fill: "red"}}/> :
+                            <FavoriteBorderOutlinedIcon onClick={() => toggleLike()} className={`${styles.Icon} ${styles.Like}`}/>}
                     </div>
                     <div className={`${styles.iconWithText} ${styles.projectComments}`}>
-                        <span>{project.data.comments_count}</span>
+                        <span>{projectData.data.comments_count}</span>
                         <Comment className="icon"/>
                     </div>
                 </div>
